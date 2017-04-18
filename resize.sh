cf0944133249fd00cb0523d0beead0bf02eb9561
Fabian Schlenz <mail@fabianonline.de>
2017-04-18 10:26:17 +0000

    resize.sh won't fail if there aren't enough images to fill the new image.

--- resize.sh
+++ resize.sh
@@ -35,12 +35,13 @@ offs=$square_offset
 bord=$border_width
 for i in ${sources[@]}; do
 	base=${i:0:$name_offset}
-	convert -size $[size*2 + bord*3]x$[size*2 + bord*3] xc:white \
-		${base}---1.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord] -composite \
-		${base}---2.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord*2+size]+$[bord] -composite \
-		${base}---3.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*2+size] -composite \
-		${base}---4.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord*2+size]+$[bord*2+size] -composite \
-		merged/square/$(basename "${base}").jpg
+	cmd="convert -size $[size*2 + bord*3]x$[size*2 + bord*3] xc:white "
+	[ -f "${base}---1.jpg" ] && cmd="$cmd ${base}---1.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord] -composite "
+	[ -f "${base}---2.jpg" ] && cmd="$cmd ${base}---2.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord*2+size]+$[bord] -composite "
+	[ -f "${base}---3.jpg" ] && cmd="$cmd ${base}---3.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*2+size] -composite "
+	[ -f "${base}---4.jpg" ] && cmd="$cmd ${base}---4.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord*2+size]+$[bord*2+size] -composite "
+	cmd="$cmd merged/square/$(basename "${base}").jpg"
+	$cmd
 	echo -n "."
 done
 echo " done."
@@ -48,12 +49,13 @@ echo " done."
 echo "Creating line images..."
 for i in ${sources[@]}; do
 	base=${i:0:$name_offset}
-	convert -size $[size + bord*2]x$[size*4 + bord*5] xc:white \
-		${base}---1.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*1+size*0] -composite \
-		${base}---2.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*2+size*1] -composite \
-		${base}---3.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*3+size*2] -composite \
-		${base}---4.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*4+size*3] -composite \
-		merged/line/$(basename "${base}").jpg
+	cmd="convert -size $[size + bord*2]x$[size*4 + bord*5] xc:white "
+	[ -f "${base}---1.jpg" ] && cmd="$cmd ${base}---1.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*1+size*0] -composite "
+	[ -f "${base}---2.jpg" ] && cmd="$cmd ${base}---2.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*2+size*1] -composite "
+	[ -f "${base}---3.jpg" ] && cmd="$cmd ${base}---3.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*3+size*2] -composite "
+	[ -f "${base}---4.jpg" ] && cmd="$cmd ${base}---4.jpg[${size}x${size}+${offs}+0] -geometry ${size}x${size}+$[bord]+$[bord*4+size*3] -composite "
+	cmd="$cmd merged/line/$(basename "${base}").jpg"
+	$cmd
 	echo -n "."
 done
 echo " done."
@@ -65,11 +67,12 @@ size_h=$[size*4 + bord*5]
 i=0
 while [[ $i -lt ${#files[@]} ]] ; do
 	file=${files[$i]}
-	convert -size $[size_w*3]x${size_h} xc:white \
-		${files[$i]}   -geometry ${size_w}x${size_h}+$[size_w*0]+0 -composite \
-		${files[$i+1]} -geometry ${size_w}x${size_h}+$[size_w*1]+0 -composite \
-		${files[$i+2]} -geometry ${size_w}x${size_h}+$[size_w*2]+0 -composite \
-		merged/3lines/$(basename "${file}")
+	cmd="convert -size $[size_w*3]x${size_h} xc:white "
+	[ -f "${files[$i]}" ] && cmd="$cmd ${files[$i]}   -geometry ${size_w}x${size_h}+$[size_w*0]+0 -composite "
+	[ -f "${files[$i+1]}" ] && cmd="$cmd ${files[$i+1]} -geometry ${size_w}x${size_h}+$[size_w*1]+0 -composite "
+	[ -f "${files[$i+2]}" ] && cmd="$cmd ${files[$i+2]} -geometry ${size_w}x${size_h}+$[size_w*2]+0 -composite "
+	cmd="$cmd merged/3lines/$(basename "${file}")"
+	$cmd
 	echo -n "."
 	i=$( expr $i + 3 )
 done
